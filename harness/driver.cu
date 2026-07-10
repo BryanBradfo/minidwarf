9d867ed646e02e95139c75b3c08b78fd18cddf3c
Bryan Chen <bryanchen1105@gmail.com>
2026-07-10 16:07:33 +0200

    Add CUDA error checking to driver

--- harness/driver.cu
+++ harness/driver.cu
@@ -6,6 +6,14 @@
 
 extern "C" void minidwarf_solve(const float* const*, float* const*, const long*, int);
 
+#define CUDA_CHECK(call) do { \
+    cudaError_t _e = (call); \
+    if (_e != cudaSuccess) { \
+      fprintf(stderr, "CUDA error at %s:%d: %s\n", __FILE__, __LINE__, cudaGetErrorString(_e)); \
+      exit(1); \
+    } \
+  } while (0)
+
 static long prod(const std::vector<long>& d){ long p=1; for(long x:d) p*=x; return p; }
 
 int main(int argc, char** argv){
@@ -15,28 +23,31 @@ int main(int argc, char** argv){
   long n = prod(dims);
 
   FILE* f=fopen(in,"rb");
+  if(!f){ fprintf(stderr, "cannot open %s\n", in); exit(1); }
   std::vector<float*> din(n_in), dout(n_out);
   for(int i=0;i<n_in;i++){ std::vector<float> h(n); fread(h.data(),4,n,f);
-    cudaMalloc(&din[i], n*4); cudaMemcpy(din[i],h.data(),n*4,cudaMemcpyHostToDevice);}
+    CUDA_CHECK(cudaMalloc(&din[i], n*4)); CUDA_CHECK(cudaMemcpy(din[i],h.data(),n*4,cudaMemcpyHostToDevice));}
   fclose(f);
-  for(int i=0;i<n_out;i++) cudaMalloc(&dout[i], n*4);
+  for(int i=0;i<n_out;i++) CUDA_CHECK(cudaMalloc(&dout[i], n*4));
 
   minidwarf_solve(din.data(), dout.data(), dims.data(), (int)dims.size()); // warmup
-  cudaDeviceSynchronize();
+  CUDA_CHECK(cudaDeviceSynchronize());
+  CUDA_CHECK(cudaGetLastError());
 
-  cudaEvent_t s,e; cudaEventCreate(&s); cudaEventCreate(&e);
+  cudaEvent_t s,e; CUDA_CHECK(cudaEventCreate(&s)); CUDA_CHECK(cudaEventCreate(&e));
   std::vector<float> times(reps);
-  for(int r=0;r<reps;r++){ cudaEventRecord(s);
+  for(int r=0;r<reps;r++){ CUDA_CHECK(cudaEventRecord(s));
     minidwarf_solve(din.data(), dout.data(), dims.data(), (int)dims.size());
-    cudaEventRecord(e); cudaEventSynchronize(e);
-    cudaEventElapsedTime(&times[r], s, e); }
+    CUDA_CHECK(cudaEventRecord(e)); CUDA_CHECK(cudaEventSynchronize(e));
+    CUDA_CHECK(cudaEventElapsedTime(&times[r], s, e)); }
   for(size_t i=0;i+1<times.size();i++) for(size_t j=i+1;j<times.size();j++)
     if(times[j]<times[i]) std::swap(times[i],times[j]);
   float med = times[reps/2];
 
   FILE* fo=fopen(out,"wb");
+  if(!fo){ fprintf(stderr, "cannot open %s\n", out); exit(1); }
   for(int i=0;i<n_out;i++){ std::vector<float> h(n);
-    cudaMemcpy(h.data(),dout[i],n*4,cudaMemcpyDeviceToHost); fwrite(h.data(),4,n,fo);}
+    CUDA_CHECK(cudaMemcpy(h.data(),dout[i],n*4,cudaMemcpyDeviceToHost)); fwrite(h.data(),4,n,fo);}
   fclose(fo);
   FILE* ft=fopen(tj,"w"); fprintf(ft,"{\"median_ms\": %f}\n", med); fclose(ft);
   return 0;
